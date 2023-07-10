5c7bc40e9a91cf8c1557d0b24b31152a9e5c6f72
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-10 18:23:23 +0200

    add PageCard (not complete)

--- src/components/PageCard.vue
+++ src/components/PageCard.vue
@@ -2,10 +2,201 @@
 export default {
     data() {
         return {
-
         }
     }
 }
 </script>
-<template></template>
-<style scoped></style>
+<template>
+    <section id="card">
+        <h2 class="text-center py-5">New courses</h2>
+        <div class="container">
+            <div class="row height">
+                <div class="col-4 d-flex justify-content-end align-items-start">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/images/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">How to be a DJ? Make Eletronic Music</h5>
+                            <p class="card-text text-center course">Eletronic</p>
+                            <div class="separator"></div>
+                            <div class="row sub-height">
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-signal me-1"></i>
+                                    <p class="m-0 sub-text">Advance</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center ps-1">
+                                    <i class="fa-solid fa-list-check me-1"></i>
+                                    <p class="m-0 sub-text">8 Lectures</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-clock me-1"></i>
+                                    <p class="m-0 sub-text">6 hours</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-start">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/images/12345-1-370x200.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Nvidia and UE4 Techonolgies Practice</h5>
+                            <p class="card-text text-center course">Nvidia</p>
+                            <div class="separator"></div>
+                            <div class="row sub-height">
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-signal me-1"></i>
+                                    <p class="m-0 sub-text">Advance</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center ps-1">
+                                    <i class="fa-solid fa-list-check me-1"></i>
+                                    <p class="m-0 sub-text">8 Lectures</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-clock me-1"></i>
+                                    <p class="m-0 sub-text">6 hours</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex justify-content-start align-items-start">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/images/photo-1491897554428-130a60dd4757-370x200.jpeg" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Fashion Photography from professional</h5>
+                            <p class="card-text text-center course">Fashion</p>
+                            <div class="separator"></div>
+                            <div class="row sub-height">
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-signal me-1"></i>
+                                    <p class="m-0 sub-text">Advance</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center ps-1">
+                                    <i class="fa-solid fa-list-check me-1"></i>
+                                    <p class="m-0 sub-text">6 Lectures</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-clock me-1"></i>
+                                    <p class="m-0 sub-text">6 hours</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex justify-content-end align-items-start">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/images/photo-1416339134316-0e91dc9ded92-370x200.jpeg" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Design Instruments for Communication</h5>
+                            <p class="card-text text-center course">Communication</p>
+                            <div class="separator"></div>
+                            <div class="row sub-height">
+                                <div class="col-4 d-flex align-items-center ps-0">
+                                    <i class="fa-solid fa-signal me-1"></i>
+                                    <p class="m-0 sub-text">Intermediate</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center ps-1">
+                                    <i class="fa-solid fa-list-check me-1"></i>
+                                    <p class="m-0 sub-text">6 Lectures</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-clock me-1"></i>
+                                    <p class="m-0 sub-text">6 hours</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-start">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/images/cathryn-lavery-67852-unsplash-370x200.jpg" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Make your Concept Right and Beatiful</h5>
+                            <p class="card-text text-center course">Art</p>
+                            <div class="separator"></div>
+                            <div class="row sub-height">
+                                <div class="col-4 d-flex align-items-center ps-0">
+                                    <i class="fa-solid fa-signal me-1"></i>
+                                    <p class="m-0 sub-text">Intermediate</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center ps-1">
+                                    <i class="fa-solid fa-list-check me-1"></i>
+                                    <p class="m-0 sub-text">6 Lectures</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-clock me-1"></i>
+                                    <p class="m-0 sub-text">6 hours</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex justify-content-start align-items-start">
+                    <div class="card" style="width: 18rem;">
+                        <img src="../assets/images/photo-1475452779376-caebfb988090-370x200.jpeg" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Road Bike Manueal or How to Be a Champions</h5>
+                            <p class="card-text text-center course">Bicycling</p>
+                            <div class="separator"></div>
+                            <div class="row sub-height">
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-signal me-1"></i>
+                                    <p class="m-0 sub-text">Begginer</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center ps-1">
+                                    <i class="fa-solid fa-list-check me-1"></i>
+                                    <p class="m-0 sub-text">6 Lectures</p>
+                                </div>
+                                <div class="col-4 d-flex align-items-center">
+                                    <i class="fa-solid fa-clock me-1"></i>
+                                    <p class="m-0 sub-text">6 hours</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+#card {
+    background-color: #edf2f5;
+}
+
+.height {
+    height: 850px;
+}
+
+.sub-height {
+    height: 30px;
+}
+
+h2 {
+    font-size: 40px;
+    font-weight: bold;
+}
+
+.course {
+    color: #aaaaaa;
+}
+
+.separator {
+    height: 1px;
+    border: 1px solid #e9e9e9;
+}
+
+.sub-text {
+    font-size: 0.8rem;
+}
+
+.fa-solid {
+    font-size: 0.8rem;
+    color: #fcb900;
+}
+</style>
