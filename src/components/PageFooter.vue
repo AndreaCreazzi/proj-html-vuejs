a88cc0f539f27315806cb4bf577e61390f445ddc
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-11 17:56:59 +0200

    add PageFooter (not complete)

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -5,5 +5,36 @@ export default {
     }
 }
 </script>
-<template></template>
-<style scoped></style>
+<template>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <h3>ABOUT</h3>
+                </div>
+                <div class="col-3">
+                    <H3>CONTACT</H3>
+                </div>
+                <div class="col-3">
+                    <H3>PAGES</H3>
+                </div>
+                <div class="col-3">
+                    <H3>BLOG</H3>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<style scoped>
+footer {
+    background-color: #fcb900;
+}
+
+.row {
+    height: 500px;
+}
+
+h3 {
+    padding-top: 70px;
+}
+</style>
