855ce73d4bddf322b228fdd5eefd85dc53cc8849
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-12 12:51:54 +0200

    add PageFooter (complete)

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,30 @@ export default {
           url: '#',
           current: true,
         }
+      ],
+      aboutInfo: {
+        site: 'Masterstudy is Education WordPress theme featured by Learning.',
+        company: 'Management System (LMS) for online education',
+        developerCompany: 'Developed by StylemixThemes'
+      },
+      contactsInfo: {
+        place: 'USA, Callifornia 20, First Avenue, Callifornia',
+        phone: '+1 212 458 300 32',
+        fax: '+1 212 375 24 14',
+        email: 'info@masterstudy.com'
+      },
+      pagesInfo: ['blog', 'Courses', 'Home', 'Membership A..', 'Shortcodes', 'Typography'],
+      blogInfo: [
+        {
+          image: 'photo-1517520287167-4bbf64a00d66-150x150.jpeg',
+          title: 'Our main target is to "Developing Yourself as a Leader"',
+          text: 'August 9, 2018'
+        },
+        {
+          image: 'photo-1490376840453-5f616fbebe5b-150x150.jpeg',
+          title: 'Those Other College Expenses You Are Not Thinking About',
+          text: 'June 3, 2015'
+        }
       ]
     }
   }
@@ -41,6 +65,6 @@ export default {
 <template>
   <PageHeader :logoHeader="logoHeader" :navbarOptions="navbarOptions" />
   <PageMain />
-  <PageFooter />
+  <PageFooter :aboutInfo="aboutInfo" :contactsInfo="contactsInfo" :pagesInfo="pagesInfo" :blogInfo="blogInfo" />
 </template>
 <style></style>
--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -2,6 +2,12 @@
 export default {
     data() {
         return {}
+    },
+    props: {
+        aboutInfo: Object,
+        contactsInfo: Object,
+        pagesInfo: Array,
+        blogInfo: Array
     }
 }
 </script>
@@ -11,15 +17,37 @@ export default {
             <div class="row">
                 <div class="col-3">
                     <h3>ABOUT</h3>
+                    <p class="about-first">{{ aboutInfo.site }}</p>
+                    <p>{{ aboutInfo.company }}</p>
+                    <p>{{ aboutInfo.developerCompany }}</p>
+                    <div class="d-flex brands">
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
                 </div>
                 <div class="col-3">
-                    <H3>CONTACT</H3>
+                    <h3>CONTACT</h3>
+                    <p class="contacts-first">{{ contactsInfo.place }}</p>
+                    <p>{{ contactsInfo.phone }}</p>
+                    <p>{{ contactsInfo.fax }}</p>
+                    <p class="contacts-last">{{ contactsInfo.email }}</p>
                 </div>
                 <div class="col-3">
-                    <H3>PAGES</H3>
+                    <h3>PAGES</h3>
+                    <ul class="d-flex flex-wrap">
+                        <li v-for="page in pagesInfo"><a>{{ page }}</a></li>
+                    </ul>
                 </div>
                 <div class="col-3">
-                    <H3>BLOG</H3>
+                    <h3>BLOG</h3>
+                    <div v-for="blog in blogInfo" :key="blog.title" class="d-flex">
+                        <img :src="`/src/assets/images/${blog.image}`" alt="">
+                        <div class="d-flex flex-column right-description">
+                            <h4>{{ blog.title }}</h4>
+                            <p class="blockquote-footer">{{ blog.text }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -36,5 +64,56 @@ footer {
 
 h3 {
     padding-top: 70px;
+    margin-bottom: 50px;
+}
+
+p {
+    margin-bottom: 0;
+}
+
+.about-first {
+    padding-right: 10px;
+}
+
+.brands {
+    margin-top: 20px;
+}
+
+.fa-brands {
+    margin-right: 20px;
+}
+
+.contacts-first {
+    margin-bottom: 20px;
+    padding-right: 10px;
+}
+
+.contacts-last {
+    margin-top: 20px;
+}
+
+ul li {
+    flex-basis: calc(100% / 2);
+    margin-bottom: 20px;
+}
+
+ul li a {
+    cursor: pointer;
+}
+
+img {
+    height: 80px;
+    width: 80px;
+    margin-bottom: 35px;
+}
+
+h4 {
+    font-size: 15px;
+    margin-bottom: 20px;
+    padding-right: 70px;
+}
+
+.right-description {
+    margin-left: 10px;
 }
 </style>
