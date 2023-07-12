77fce303994c18db32e01bc3d8961a234537e87b
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-12 13:07:06 +0200

    add hover + fix PageHeader

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -19,6 +19,7 @@ export default {
                 </div>
                 <div class="col-6 start">
                     <ul class="d-flex m-0 p-0 pe-5">
+                        <p class="special">NEW</p>
                         <li class="list-unstyled mx-2" v-for="option in navbarOptions" :key="option.text"><a
                                 :class="{ 'active': option.current }" :url="option.url">{{
                                     option.text }}</a></li>
@@ -44,16 +45,38 @@ export default {
     justify-content: start;
 }
 
+ul {
+    position: relative;
+}
+
+.special {
+    position: absolute;
+    bottom: 0%;
+    right: 36%;
+    font-size: 7px;
+    padding: 1px 5px;
+    background-color: #dc1245;
+    color: white;
+    border-radius: 3px;
+    font-weight: bold;
+}
+
 ul li a {
     text-decoration: none;
     color: black;
     padding-bottom: 0.5rem;
     font-weight: bold;
+    cursor: pointer;
 }
 
 ul li a:hover,
 .active {
     border-bottom: 2px solid #fcb900;
+}
+
+i:hover {
+    color: #fcb900;
+    cursor: pointer;
 }
 
 .fa-brands {
